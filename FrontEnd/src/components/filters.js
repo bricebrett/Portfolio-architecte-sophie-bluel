ca622276245381186c123de6c4c1b456bb851932
Brice <brettinb@hotmail.fr>
2025-01-28 14:05:54 +0100

    feat: add data-set-attribute for filters

--- FrontEnd/src/components/filters.js
+++ FrontEnd/src/components/filters.js
@@ -4,3 +4,17 @@ export const filterWorksByCategory = (works, categoryId) => {
     const filteredWorks = works.filter(work => work.categoryId == categoryId);
     addWorks(filteredWorks);
 };
+
+// Ajoutez un écouteur d'événements pour les boutons de catégorie
+document.querySelectorAll('.btn-categorie').forEach(button => {
+    button.addEventListener('click', (event) => {
+        const categoryId = event.target.dataset.categoryId;
+        const works = Array.from(document.querySelectorAll('.work')).map(workElement => ({
+            id: workElement.dataset.id,
+            categoryId: workElement.dataset.categoryId,
+            imageUrl: workElement.querySelector('img').src,
+            title: workElement.querySelector('.work-info h3').textContent
+        }));
+        filterWorksByCategory(works, categoryId);
+    });
+});
--- FrontEnd/src/components/works.js
+++ FrontEnd/src/components/works.js
@@ -1,18 +1,20 @@
 /**
  * Function add works to HTML
- * */ 
+ */
 export const addWorks = (works) => {
     const worksContainer = document.querySelector('#gallery');
     worksContainer.innerHTML = '';
-    works.forEach( work => {
+    works.forEach(work => {
         const workElement = document.createElement('div');
         workElement.classList.add('work');
+        workElement.dataset.id = work.id;
+        workElement.dataset.categoryId = work.categoryId;
         workElement.innerHTML = `
-        <img src='${work.imageUrl}' alt='${work.title}' />
-        <div class='work-info'>
-            <h3>${work.title}</h3>
-        </div>
+            <img src='${work.imageUrl}' alt='${work.title}' />
+            <div class='work-info'>
+                <h3>${work.title}</h3>
+            </div>
         `;
-    worksContainer.appendChild(workElement);
+        worksContainer.appendChild(workElement);
     });
 }
